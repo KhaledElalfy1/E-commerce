bda4aee61f908469483d2bb8880b706af73fb769
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-02-19 19:02:12 +0200

    finish complete profile cubit

--- lib/features/complete_profile/presentation/controller/cubit/complete_profile_cubit.dart
+++ lib/features/complete_profile/presentation/controller/cubit/complete_profile_cubit.dart
@@ -1,8 +1,26 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'complete_profile_state.dart';
 
 class CompleteProfileCubit extends Cubit<CompleteProfileState> {
+  static CompleteProfileCubit get(context) => BlocProvider.of(context);
+
   CompleteProfileCubit() : super(CompleteProfileInitial());
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController phoneNameController = TextEditingController();
+  TextEditingController addressNameController = TextEditingController();
+  List<String> errors = [];
+  GlobalKey<FormState> globalKey = GlobalKey();
+
+  void addErrorToList(String error) {
+    errors.add(error);
+    emit(CompleteProfileUpdateList());
+  }
+
+  void removeError(String error) {
+    errors.remove(error);
+    emit(CompleteProfileUpdateList());
+  }
 }
